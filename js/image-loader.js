25526b3921a263b6d2df35314b76eaeada12c548
Sepehr Amoor Pour <sepehr.amoor.pour@gmail.com>
2016-02-06 12:57:30 +0100

    Initial commit of Angular module and package.json

---
+++ js/image-loader.js
@@ -0,0 +1,45 @@
+/*!
+ * Image Loader Angular v1.0.0
+ * (c) 2016 Sepehr Amoor Pour
+ * Released under the MIT License.
+ */
+angular.module('sap.imageloader', [])
+
+.factory('ImageLoader', ['$q', function($q){
+	var imageLoader = {
+		loadImages: function (images, srcProperty) {
+			srcProperty = typeof srcProperty !== "undefined" ? srcProperty : "src";
+			var promises = [];
+			for (var i = images.length - 1; i >= 0; i--) {
+				var image = this.loadImage(images[i]);
+				promises.push(image);
+			};
+
+			return $q.all(promises);
+		},
+
+		loadImage: function(image, srcProperty) {
+			var deferred = $q.defer()
+			var imageObject = new Image();
+			imageObject.onload = function() {
+				if (typeof image === "object") {
+					image[srcProperty] = imageObject.src;
+				}
+				else if (typeof image === "string") {
+					image = imagesObject.src;
+				}
+				deferred.resolve(image);
+				return deferred.promise;
+			};
+
+			if (typeof image === "object") {
+				imageObject.src = image[srcProperty]
+			}
+			else if (typeof image === "string") {
+				imagesObject.src = image;
+			}
+		}
+	};
+
+	return imageLoader;
+}]);
